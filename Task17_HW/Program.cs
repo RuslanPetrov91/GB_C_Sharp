4ddda99ac16acda50c8a897e58ee531cb743a807
Ruslan Petrov <ruslan.petrov01031991@gmail.com>
2023-07-14 18:36:12 +0200

    add_Task17_HW

---
+++ Task17_HW/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное
+// число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int ReadInt(string message)
+{
+    System.Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+bool ThreeDigitNumber(int number)
+{
+    if (number >= 100 && number < 1000)
+    {
+        return true;
+    }
+    System.Console.WriteLine("Вы ввели не трехзначное число");
+    return false;
+}
+
+int num = ReadInt("Введите число > ");
+int value = 0;
+
+if (ThreeDigitNumber(num))
+{
+    num = num / 10;
+    value = num % 10;
+    System.Console.WriteLine($"Вторая цифра числа равна {value}");
+}
