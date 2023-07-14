803b48cb30b5d62b01baed1e3c13c754b73a6ede
Ruslan Petrov <ruslan.petrov01031991@gmail.com>
2023-07-14 19:05:17 +0200

    add_Task27_HW

---
+++ Task27_HW/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int number = Prompt("Введите число => ");
+int result = 1;
+
+int Prompt(string message)
+{
+    Console.Write(message);                     // печатается на консоль
+    int num = int.Parse(Console.ReadLine()!);   // конвертируется
+    return num;                                 // возврат
+}
+
+int CubeNumber(int number, int result)
+{
+    while (result <= number)
+    {
+        System.Console.WriteLine($"{result * result * result}");
+        result++;
+    }
+
+    return result;
+}
+
+result = CubeNumber(number, result);
